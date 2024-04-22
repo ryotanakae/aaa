cc26cafed5f32a38d04903582b812c5468e291c9
EC2 Default User <ec2-user@ip-172-31-23-107.ap-northeast-1.compute.internal>
2024-04-22 20:42:38 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new,  :index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_22_072513) do
+ActiveRecord::Schema.define(version: 2024_04_22_201609) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2024_04_22_072513) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,4 +65,5 @@ ActiveRecord::Schema.define(version: 2024_04_22_072513) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "books", "users"
 end
